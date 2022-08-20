2eb3928b18db4b148a7a4022d32f17ff22fbf3dd
Johnny <nor0x@users.noreply.github.com>
2022-08-20 19:21:25 +0200

    WTF

---
+++ MauiFails.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiFails.Lib
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ MauiFails.Lib/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiFails.Lib
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
