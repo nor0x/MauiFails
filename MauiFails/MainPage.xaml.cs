0c227dea0745c2cc2d45005d2db9cd9c0affbdc4
Johnny <nor0x@users.noreply.github.com>
2022-08-28 21:42:08 +0200

    we can click 🖱️👆

--- MauiFails/MainPage.xaml.cs
+++ MauiFails/MainPage.xaml.cs
@@ -9,7 +9,8 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	Random rand = new Random();
+	private async void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
 
@@ -19,6 +20,42 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		await MovingBtnGrid.TranslateTo(rand.Next(0, 200), rand.Next(0, 200), 250, Easing.BounceOut);
+		MovingBtn.WidthTo(rand.Next(30, 200), 400, Easing.BounceIn);
 	}
+
+	private void OnMovingButtonClicked(object sender, EventArgs e)
+	{
+		MovingBtn.Text = DateTime.Now.ToString();
+		Console.WriteLine("click");
+    }
+
+	private void BorderTapped(object sender, EventArgs e)
+	{
+		Console.WriteLine("border tapped");
+    }
 }
 
+public static class Extensions
+{
+    public static async Task<bool> HeightTo(this View view, double height, uint duration = 250, Easing easing = null)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+
+        var heightAnimation = new Animation(x => view.HeightRequest = x, view.Height, height);
+        heightAnimation.Commit(view, "HeightAnimation", 10, duration, easing, (finalValue, finished) => { tcs.SetResult(finished); });
+
+        return await tcs.Task;
+    }
+
+    public static async Task<bool> WidthTo(this View view, double width, uint duration = 250, Easing easing = null)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+
+        var heightAnimation = new Animation(x => view.WidthRequest = x, view.Height, width);
+        heightAnimation.Commit(view, "WidthAnimation", 10, duration, easing, (finalValue, finished) => { tcs.SetResult(finished); });
+
+        return await tcs.Task;
+    }
+}
